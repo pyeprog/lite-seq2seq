fa8fbe62089cd4f9142292cbaea0dc6d62c22189
Pye <pyeprog@foxmail.com>
2018-09-13 14:21:52 +0800

    recover dataset and add seq length filter

--- data/movie_dialogs/parse.py
+++ data/movie_dialogs/parse.py
@@ -12,23 +12,26 @@ conv_list = []
 
 n_lines = len(raw_lines)
 for i, line in enumerate(raw_lines):
-    print('{}/{} Line processing ...'.format(i+1, n_lines))
+    print('\r{}/{} Line processing ...'.format(i+1, n_lines), end='', flush=True)
     line_info = line.split(' +++$+++ ')
     line_dict[line_info[0]] = line_info[-1]
         
+print()
 
 n_conversation = len(raw_conversation)
 for i, conv in enumerate(raw_conversation):
-    print('{}/{} conversation processing ...'.format(i+1, n_conversation))
+    print('\r{}/{} conversation processing ...'.format(i+1, n_conversation), end='', flush=True)
     conv_list.append(eval(conv.split(' +++$+++ ')[-1])) 
 
 
 question_list = []
 answer_list = []
 
+print()
+
 n_conv_list = len(conv_list)
 for c, conv in enumerate(conv_list):
-    print('{}/{} Parsing questions and answers ...'.format(c+1, n_conv_list))
+    print('\r{}/{} Parsing questions and answers ...'.format(c+1, n_conv_list), end='', flush=True)
     for i in range(len(conv)-1):
         question_list.append(line_dict[conv[i]])
         answer_list.append(line_dict[conv[i+1]])
